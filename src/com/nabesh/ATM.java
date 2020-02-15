cdb3220e28c7b77144b1ef53000c764e4712c8d5
KariukiAbel <abelmuriithi@gmail.com>
2020-02-15 14:58:55 +0300

    Began an ATM banking project.Added an ATM,Keypad, Screen, Cashdispenser, Depositslot and Bankdatabase classes.

---
+++ src/com/nabesh/ATM.java
@@ -0,0 +1,11 @@
+package com.nabesh;
+
+public class ATM {
+    private boolean userAuthenticated;
+    private int currentAccountNumber;
+    private Screen screen;
+    private Keypad keypad;
+    private CashDispenser cashDispenser;
+    private DepositSlot depositSlot;
+    private BankDataBase bankDataBase;
+}
